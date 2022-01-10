fbe695ea5dc53d6bbccb0c606b90e4d1cbed102f
Pierce England <pengland1@nait.ca>
2022-01-10 15:38:45 -0700

    Added Variable project

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intValue;
+
+            intValue = 5;
+
+            Console.WriteLine("This value is: ");
+            Console.WriteLine(intValue);
+        }
+    }
+}
