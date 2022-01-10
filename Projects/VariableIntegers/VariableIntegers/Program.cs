e1d258035333c81d3849dbdd96a54c4fec04b8e8
Pierce England <pengland1@nait.ca>
2022-01-10 16:01:01 -0700

    added VariableInteger project

---
+++ Projects/VariableIntegers/VariableIntegers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariableIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chequing;
+            long days;
+
+            chequing = 20;
+            days = 189000;
+
+            Console.WriteLine($"Our account balance is {chequing:C}");
+            Console.WriteLine($"About {days} days ago Columbus stood on this spot.");
+        }
+    }
+}
