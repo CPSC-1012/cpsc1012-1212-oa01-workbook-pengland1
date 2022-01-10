da87b3f741578ed23e0716516c9ecab994da34c3
Pierce England <pengland1@nait.ca>
2022-01-10 15:24:44 -0700

    Added Tab project.

---
+++ Tab/Tab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("These are the top sellers:");
+            Console.Write("\n\tProduct 1\n\tProduct 2\n\tProduct 3\n");
+        }
+    }
+}
