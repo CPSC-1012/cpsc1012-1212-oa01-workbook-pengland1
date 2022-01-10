d62f911c23304fb4dbe8472b528080c1fcd465d9
Pierce England <pengland1@nait.ca>
2022-01-10 16:22:38 -0700

    added SaleApp

---
+++ Projects/SaleApp/SaleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SaleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double price,
+                tax,
+                total;
+
+            price = 29.99;
+            tax = price * 0.05;
+            total = price + tax;
+
+            Console.WriteLine($"The price of the item is: {price:C}");
+            Console.WriteLine($"The tax of the item is {tax:C}");
+            Console.WriteLine($"The total of the item is {total:C}");
+        }
+    }
+}
